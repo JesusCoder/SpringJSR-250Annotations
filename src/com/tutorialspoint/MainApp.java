70df6f69f48a757b67e7cd20622639f0f526a4ab
Steve Sun <jiahuan@amazon.com>
2015-10-26 06:56:52 -0700

    added SpringJSR-250Annotations project

---
+++ src/com/tutorialspoint/MainApp.java
@@ -0,0 +1,23 @@
+package com.tutorialspoint;
+
+/**Following is the content of the MainApp.java file. Here you 
+ * need to register a shutdown hook registerShutdownHook() method that 
+ * is declared on the AbstractApplicationContext class. 
+ * This will ensures a graceful shutdown and calls the relevant destroy methods.*/
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+	public static void main(String[] args) {
+
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext(
+				"com/tutorialspoint/Beans.xml");
+
+		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+		obj.getMessage();
+		context.registerShutdownHook();
+
+		System.out.print("\nThe end!\n");
+	}
+}
